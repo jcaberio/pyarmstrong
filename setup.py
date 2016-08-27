3d51324dcc6146de4d61daa71dc445fb09ad7897
Jorick Caberio <jorick.caberio@voyagerinnovation.com>
2016-08-27 19:37:51 +0000

    initial commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup, Extension
+
+setup(name='pyarmstrong',
+      version = '0.1',
+      description = 'Simple C extension for checking armstrong numbers.',
+      author = 'Jorick Caberio',
+      author_email = 'jorick.caberio@voyagerinnovation.com'
+      ext_modules=[
+        Extension('pyarmstrong',
+                  sources =  ['armstrong.c'],
+                  include_dirs = ['/usr/include'],
+                  library_dirs = ['/usr/lib'],
+                  )
+      ]
+)
